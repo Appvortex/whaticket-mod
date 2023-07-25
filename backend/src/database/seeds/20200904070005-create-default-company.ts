8bc6ec4a5037415b981b6767f61c0bce092f6dea
higordevv <pedrohigor2004@gmail.com>
2023-07-25 17:45:12 -0300

    Seeds novas

--- backend/src/database/seeds/20200904070005-create-default-company.ts
+++ backend/src/database/seeds/20200904070005-create-default-company.ts
@@ -8,11 +8,20 @@ module.exports = {
           "Plans",
           [
             {
-              name: "Plano 1",
-              users: 10,
-              connections: 10,
-              queues: 10,
-              value: 30,
+              name: "START",
+              users: 3,
+              connections: 1,
+              queues: 3,
+              value: 97,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+              {
+              name: "PREMIUM",
+              users: 6,
+              connections: 2,
+              queues: 6,
+              value: 197,
               createdAt: new Date(),
               updatedAt: new Date()
             }
@@ -23,7 +32,7 @@ module.exports = {
           "Companies",
           [
             {
-              name: "Empresa 1",
+              name: "Zappchat",
               planId: 1,
               dueDate: "2093-03-14 04:00:00+01",
               createdAt: new Date(),
